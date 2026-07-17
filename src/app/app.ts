ac939641a4cd3aca117b492486f7f268823816ff
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-07-17 19:16:53 -0300

    fix(app): corrige erro de português no título da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router'; //remove a importação de RouterOurtlet, pois não é necessário para este componente
 //import {Produto} from './components/produto/produto' //importando a classe produto do arquivo app.ts
-import { ListaProdutos } from './components/lista-produtos/lista-produtos';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
