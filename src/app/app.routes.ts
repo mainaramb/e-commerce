40fb76d4ce762a6b5ae7802e46a7d1fd50a73fbe
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-07-22 21:48:07 -0300

    Refactor(routes): adiciona rotas para home, produtos e carrinho protegido

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,28 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
-import { Carrinho } from './features/carrinho/carrinho/carrinho';
+import { Routes } from "@angular/router";
+import { authGuard } from "./core/auth.guard";
 
 export const routes: Routes = [
-{
-    path:'',
-    component: ListaProdutos,
-},
-{
-    path:'carrinho',
-    component: Carrinho,
-},
+    {
+        path:'', //!router para raiz localhost:4200/
+        loadComponent: () =>
+            import('./features/home/home/home')
+        .then((m) => m.Home), 
+    },
+    {
+        path:'produtos',
+        loadComponent: () =>
+            import('./features/produtos/lista-produtos/lista-produtos')
+        .then((m) => m.ListaProdutos),
+    },
+    {
+        path:'carrinho',
+        canActivate: [authGuard],
+        loadComponent: () =>
+            import('./features/carrinho/carrinho/carrinho')
+        .then((m) => m.Carrinho),
+    },
+    {
+        path:'**',
+        redirectTo: '',
+    },
 ];
