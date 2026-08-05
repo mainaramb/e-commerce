3c190c24692742b70c5fae4e95b2b73b728211ac
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-05 19:31:33 -0300

    feat(produtos):cria método adicionarAoCarrinho() para emitir produto

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -17,4 +17,16 @@ export class Produto {
   selecionarProduto(){
     this.produtoSelecionado.emit(this.nome);
   }
+
+@Output() produtoAdicionado = new EventEmitter<{
+  nome: string;
+  preco: number;
+}>();
+
+adicionarAoCarrinho() {
+  this.produtoAdicionado.emit({
+    nome:this.nome,
+    preco:this.preco,
+  });
+}
 }
