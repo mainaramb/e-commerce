8d42f46991c151bfc1e669bbe5d38195d7230639
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-11 21:39:03 -0300

    feat(checkout): adiciona possibilidade de validez de dados em nome, endereço e email

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import { Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { CarrinhoService } from '../../../core/services/carrinho.service'; 
 
 @Component({
@@ -12,13 +13,28 @@ export class Checkout {
   carrinhoService = inject(CarrinhoService);
 
   formulario = new FormGroup({
-    nome: new FormControl(''),
-    email: new FormControl(''),
-    endereco: new FormControl(''),
+    nome: new FormControl('',[Validators.required,Validators.minLength(2), nomeSemNumeros]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    endereco: new FormControl('',[Validators.required, Validators.minLength(5)]),
   })
 
   finalizar(){
-    console.log('Dados do Formulário: ', this.formulario.value);
-    console.log('Itens no Carrinho: ', this.carrinhoService.itens());
+    if(this.formulario.invalid){
+      console.log('Formulário Invalido!');
+      return;
+    }
+    const dados = this.formulario.value;
+    const itens = this.carrinhoService.itens();
+
+    console.log('Dados do Formulário: ', dados);
+    console.log('Itens no Carrinho: ', itens);
   } 
+}
+function nomeSemNumeros(controle:AbstractControl): ValidationErrors | null{
+  const valor = controle.value;
+  if(!valor) return null;
+  if(/\d/.test(valor)){
+    return {numeroInvalido:true};
+  }
+  return null;
 }
