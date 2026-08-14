9b49cfbdd9730bbb8d62e8e1e5891a2d7736a2d8
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-14 20:20:51 -0300

    feat(auth): adiciona verificacao reativa e atribuicao de perfil de administrador

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, signal, computed } from "@angular/core";
 
-type PerfilUsuario = 'usuario';
+type PerfilUsuario = 'usuario' | 'admin';
 
 type Usuario ={
     email: string;
@@ -18,12 +18,15 @@ export class AuthService{
     usuarioAtual = computed(()=> this.usuario());
     usuarioLogado = computed(()=> this.usuario() !== null);
     token = computed(()=> this.tokenJwt());
+    admin = computed(() =>this.usuario()?.perfil === 'admin');
 
     login(email: string, senha: string): boolean {
 
         if(!email || !senha){
             return false
         }
+
+        const perfil: PerfilUsuario = email === 'admin@email.com.br' ? 'admin' : 'usuario';
         const tokenSimulado = 
            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
            'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
@@ -31,7 +34,7 @@ export class AuthService{
 
         this.usuario.set({
             email,
-            perfil: 'usuario',
+            perfil,
         });
         this.tokenJwt.set(tokenSimulado);
         return true;
@@ -44,5 +47,8 @@ export class AuthService{
     }
     obterToken(): string | null {
         return this.tokenJwt();
+    }
+    obterPerfil(): PerfilUsuario | null{
+        return this.usuario()?.perfil ?? null;
     }
 }
