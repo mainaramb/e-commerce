945ff20143f3d041eb2e35ffddf66d71e5fc3786
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-14 19:15:43 -0300

    chore(app): apaga import login, logout..

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,19 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router'; //remove a importação de RouterOurtlet, pois não é necessário para este componente
 //import {Produto} from './components/produto/produto' //importando a classe produto do arquivo app.ts
 import { UpperCasePipe } from '@angular/common';
-import { usuarioLogado, login, logout} from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, Header],
+  imports: [RouterOutlet, MatButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'One Piece';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
 }
