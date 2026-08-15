891752d87dab213bf57c7316e7b43d67592d1bb0
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-14 21:23:05 -0300

    feat(login): direciona usuarios com perfil de admin para a rota /admin

--- src/app/features/login/login/login.ts
+++ src/app/features/login/login/login.ts
@@ -37,6 +37,10 @@ export class Login {
       this.erroLogin.set(true);
       return;
     }
+    if(this.authService.admin()){
+      this.router.navigateByUrl('/admin');
+      return;
+    }
 
     this.router.navigateByUrl('/produtos');
   }
