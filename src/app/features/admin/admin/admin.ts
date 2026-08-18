a111d1d9535244b31ff4ef4e2c601134327769fc
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-18 20:57:15 -0300

    feat(admin): adiciona controle de perfil autenticado e acao de sair no painel

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-admin',
@@ -6,4 +8,30 @@ import { Component } from '@angular/core';
   templateUrl: './admin.html',
   styleUrl: './admin.css',
 })
-export class Admin {}
+export class Admin {
+  // faz com o usuário volte para uma página pública
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  totalProdutosCadastrados = signal(20);
+  pedidosPendentes = signal(6);
+  usuariosCadastrados = signal(7);
+
+  usuarioAtual = this.authService.usuarioAtual;
+
+  //metodo que reforça quem é o usuário
+  areaPerfil = computed(() => {
+    const usuario = this.usuarioAtual();
+
+//condiçao que mostra se o usuario está ou autenticado
+    if(!usuario){
+      return 'Nenhum usuário autenticado';
+    }
+    return `Usuário autenticado como: ${usuario.perfil}`;
+  });
+// direciona o usuário a pagina de login
+  sair(){
+    this.authService.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
