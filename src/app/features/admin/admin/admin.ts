912e977a1086b86dab36863ab872a97e4b0cac3b
Mainara Monteiro Barbosa <mainaramonteiro0@gmail.com>
2026-08-20 15:15:59 -0300

    refactor(admin): substitui AuthService por authFacade no componente

--- src/app/features/admin/admin/admin.ts
+++ src/app/features/admin/admin/admin.ts
@@ -1,6 +1,6 @@
 import { Component, computed, signal, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../../core/services/auth.service';
+import { AuthFacade } from '../../../core/facades/auth.facade';
 
 @Component({
   selector: 'app-admin',
@@ -11,13 +11,13 @@ import { AuthService } from '../../../core/services/auth.service';
 export class Admin {
   // faz com o usuário volte para uma página pública
   private router = inject(Router);
-  private authService = inject(AuthService);
+  private authFacade = inject(AuthFacade);
 
   totalProdutosCadastrados = signal(20);
   pedidosPendentes = signal(6);
   usuariosCadastrados = signal(7);
 
-  usuarioAtual = this.authService.usuarioAtual;
+  usuarioAtual = this.authFacade.usuarioAtual;
 
   //metodo que reforça quem é o usuário
   areaPerfil = computed(() => {
@@ -31,7 +31,7 @@ export class Admin {
   });
 // direciona o usuário a pagina de login
   sair(){
-    this.authService.logout();
+    this.authFacade.sair();
     this.router.navigateByUrl('/login');
   }
 }
